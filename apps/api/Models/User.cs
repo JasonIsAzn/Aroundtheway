9f3dcd358ff13564663253ec22c6a4a3cfa6fc40
Jason <lejason123@gmail.com>
2025-09-18 17:46:18 -0500

    update user model

--- apps/api/Models/User.cs
+++ apps/api/Models/User.cs
@@ -1,6 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Aroundtheway.Api.Models;
 
 public class User
 {
+    [Key]
     public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsAdmin { get; set; } = false;
+    public string Address { get; set; } = string.Empty; // TODO Improve typing
+    public string CreditCard { get; set; } = string.Empty; // TODO encrypt this
 }
