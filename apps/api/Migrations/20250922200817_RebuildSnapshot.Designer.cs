d3c483fdb87e859cd50162aab3344e4f8047a489
Jason <lejason123@gmail.com>
2025-09-22 15:13:19 -0500

    reset migration history

--- apps/api/Migrations/20250922200817_RebuildSnapshot.Designer.cs
+++ apps/api/Migrations/20250922200817_RebuildSnapshot.Designer.cs
@@ -12,18 +12,52 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Aroundtheway.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250922171625_InventoryModel")]
-    partial class InventoryModel
+    [Migration("20250922200817_RebuildSnapshot")]
+    partial class RebuildSnapshot
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.11")
+                .HasAnnotation("ProductVersion", "8.0.13")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             MySqlModelBuilderExtensions.AutoIncrementColumns(modelBuilder);
+
+            modelBuilder.Entity("Aroundtheway.Api.Models.ChatMessage", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    MySqlPropertyBuilderExtensions.UseMySqlIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<string>("BotResponse")
+                        .IsRequired()
+                        .HasMaxLength(2000)
+                        .HasColumnType("varchar(2000)");
+
+                    b.Property<DateTime>("CreatedAt")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<DateTime>("UpdatedAt")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<int>("UserId")
+                        .HasColumnType("int");
+
+                    b.Property<string>("UserMessage")
+                        .IsRequired()
+                        .HasMaxLength(1000)
+                        .HasColumnType("varchar(1000)");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("ChatMessages");
+                });
 
             modelBuilder.Entity("Aroundtheway.Api.Models.Post", b =>
                 {
@@ -117,6 +151,17 @@ namespace Aroundtheway.Api.Migrations
                         .IsUnique();
 
                     b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("Aroundtheway.Api.Models.ChatMessage", b =>
+                {
+                    b.HasOne("Aroundtheway.Api.Models.User", "User")
+                        .WithMany()
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("User");
                 });
 
             modelBuilder.Entity("Aroundtheway.Api.Models.Post", b =>
